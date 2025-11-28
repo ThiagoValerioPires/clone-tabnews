a6e1863243218b5b8f01ce989f2bbe84cb481c57
Thiago Valério Pires <thiago.valerio@gmail.com>
2025-11-28 19:13:36 +0000

    feat: add `email.js` infra module

---
+++ tests/integration/infra/email.test.js
@@ -0,0 +1,27 @@
+import email from "infra/email.js";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => orchestrator.waitForAllServices());
+describe("infra/email.js", () => {
+  test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+    await email.send({
+      from: "FinTab <contato@fintab.com.br>",
+      to: "contato@curso.dev",
+      subject: "Teste de assunto",
+      text: "Test de corpo.",
+    });
+    await email.send({
+      from: "FinTab <contato@fintab.com.br>",
+      to: "contato@curso.dev",
+      subject: "Último email enviado",
+      text: "Corpo último email.",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+    expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<contato@curso.dev>");
+    expect(lastEmail.subject).toBe("Último email enviado");
+    expect(lastEmail.text).toBe("Corpo último email.\r\n");
+  });
+});
